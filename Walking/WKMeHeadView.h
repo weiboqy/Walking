25efff094d4db27d1e1e97e31a11205baaf48428
Walking <1148641891@qq.con>
2016-04-19 23:31:42 +0800

    0419-01

---
+++ Walking/WKMeHeadView.h
@@ -0,0 +1,15 @@
+//
+//  WKMeHeadView.h
+//  Walking
+//
+//  Created by lanou on 16/4/19.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKMeHeadView : UIView
+@property (weak, nonatomic) IBOutlet UIImageView *headImage;
+@property (weak, nonatomic) IBOutlet UIButton *LoginAndRegistButton;
+
+@end
--- Walking/Walking/Base/Me/ViewControllers/WKMeViewController.m
+++ Walking/Walking/Base/Me/ViewControllers/WKMeViewController.m
@@ -7,19 +7,52 @@
 //
 
 #import "WKMeViewController.h"
+#import "WKMeHeadView.h"
 
-@interface WKMeViewController ()
+@interface WKMeViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *meTableView;
+@property (nonatomic, strong) NSMutableArray *dataArray;
 
 @end
 
 @implementation WKMeViewController
 
+//  创建头视图
+- (void)creatHeadView {
+    NSArray *views = [[NSBundle mainBundle] loadNibNamed:@"WKMeHeadView" owner:nil options:nil];
+    WKMeHeadView *meHeadView = [views lastObject];
+    meHeadView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 160);
+    _meTableView.tableHeaderView = meHeadView;
+}
+
+//  分区数组
+- (void)creatArray {
+    NSMutableArray *oneArray = [[NSMutableArray alloc] initWithObjects:@"我的收藏", nil];
+    NSMutableArray *twoArray = [[NSMutableArray alloc] initWithObjects:@"清除缓存", nil];
+    NSMutableArray *threeArray = [[NSMutableArray alloc] initWithObjects:@"夜间模式", nil];
+    NSMutableArray *fourArray = [[NSMutableArray alloc] initWithObjects:@"关于我们", nil];
+    _dataArray = [[NSMutableArray alloc] initWithObjects:oneArray,twoArray,threeArray,fourArray, nil];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor colorWithRed:245 / 255.0 green:245 / 255.0 blue:245 / 255.0 alpha:1.0];
+    
+    
+    
     // Do any additional setup after loading the view from its nib.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 4;
+}
+
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+////    static NSString *resu
+////    UITableViewCell *cell = [tableView dequ]
+//}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
