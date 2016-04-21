829ec4525e76953e76479ec357bf4a9ac8e2ecf4
lanou <1024399113@qq.com>
2016-04-21 09:46:03 +0800

    网络请求等第三方导入

---
+++ Walking/Walking/Base/Others/Tools/NetRequstManager.h
@@ -0,0 +1,29 @@
+//
+//  NetRequstManager.h
+//  Walking
+//
+//  Created by lanou on 16/4/21.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//封装网络请求
+typedef NS_ENUM(NSInteger, RequestType) {
+    GET,
+    POST
+};
+//有参无反 的 Block
+typedef void (^RequestFinish)(NSData *data);
+typedef void (^RequestError) (NSError *error);
+
+@interface NetWorkRequestManager : NSObject
+
++ (void)requestWithType:(RequestType)type urlString:(NSString *)urlString parDic:(NSDictionary *)parDic finish:(RequestFinish)finish error:(RequestError)errors;
+
+
+
+
+
+@end
---
+++ Walking/Walking/Base/Others/Tools/NetRequstManager.m
@@ -0,0 +1,17 @@
+//
+//  NetRequstManager.m
+//  Walking
+//
+//  Created by lanou on 16/4/21.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import "NetRequstManager.h"
+
+@implementation NetRequstManager
+
+
+
+
+
+@end
