8c157cbdb622057a520bfb70fe1e796b32724a6e
lanou <lanou@lanoudeMac-mini-5.local>
2016-04-24 16:28:18 +0800

    游玩指南界面搭建

---
+++ Walking/Walking/Base/World/Models/WKGuideUserModel.h
@@ -0,0 +1,13 @@
+//
+//  WKGuideUserModel.h
+//  Walking
+//
+//  Created by lanou on 16/4/23.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WKGuideUserModel : NSObject
+
+@end
---
+++ Walking/Walking/Base/World/Views/WKTableHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  WKTableHeaderView.m
+//  Walking
+//
+//  Created by lanou on 16/4/23.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import "WKTableHeaderView.h"
+
+@implementation WKTableHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
